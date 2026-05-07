8208719ac7fa74d260cf144fca1376e52994866d
Kaio <kaio357gemadao@gmail.com>
2026-05-07 08:48:09 -0300

    commit.7

---
+++ script.js
@@ -0,0 +1,25 @@
+ /*
+
+ alert("Bom dia!"); comentario aqui
+
+var nome = "Daniel"; utilizado para criar uma variável global - comentario aqui
+let idade = 17; so pode utilizar a variável depois que ela existir - comentario aqui
+const cpf = "0000.000.000-00";    variável constante, que não pode ser alterada - comentario aqui
+
+alert("Minha idade é " + idade);
+
+let idade = 18;  não é possível criar uma variável com o mesmo nome, por isso dará erro  - comentario aqui
+
+alert("Meu CPF é " + cpf);
+
+alert ("Meu nome é " + nome);
+
+const cpf ="1111.111.111-11";  não é possível alterar o valor da variável constante, por isso dará erro - comentario aqui
+
+*/
+
+const idade = 16;
+
+if (idade < 17) {
+    alert("você é menor de idade");
+}
